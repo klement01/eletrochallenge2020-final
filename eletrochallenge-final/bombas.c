5d33bc5b7ec1e717d5d7f0e31fe4dc1082e218a3
klement01 <lucasklementr@gmail.com>
2020-05-08 18:24:33 -0300

    Bombas
    Primeira versão do projeto e do sistema controlador das bombas.

---
+++ eletrochallenge-final/bombas.c
@@ -0,0 +1,114 @@
+/** Controla as bombas, de acordo com as especificaçes do desafio.
+ *  Além disso, controla as luzes indicadoras de operação e de
+ *  emergência.
+ *  Em seu estado padrão, todas as bombas estão operando, e a luz
+ *  amarela está acesa.
+ *  Caso o botão de emergência seja pressionado, as bombas são
+ *  desligadas, a luz vermelha é acessa, e a luz amarela é desligada.
+ *  Ao receber comandos do controlador principal, o número de bombas
+ *  ativas pode mudar, e a luz amarela é alterada de acordo.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "bombas.h"
+
+#ifndef _CONTROLE_PRINCIPAL
+/* Se esse arquivo for compilado diretamente, sem o sistema de
+controle principal, cria um sistema de bombas padrão, mostra seu
+estado e fecha. */
+int main(void)
+{
+    sistemaDeBombeamento* b = Bombas(NUM_BOMBAS);
+    estadoDoBombeamento(b);
+    return 0;
+}
+#endif // _CONTROLE_PRINCIPAL
+
+/* Cria e inicializa um sistema de bombeamento. No início, todas as
+bombas estão ativas. */
+sistemaDeBombeamento* Bombas(int num_bombas)
+{
+    sistemaDeBombeamento* bombas = malloc(sizeof(sistemaDeBombeamento));
+    bombas->totais = num_bombas;
+    bombas->ativas = bombas->totais;
+    bombas->estados = malloc(bombas->totais * sizeof(bool));
+    // Tenta criar uma lista para guardar os estados. Se isso falhar,
+    // retorna um apontador nulo.
+    if (bombas->estados == NULL)
+    {
+        return NULL;
+    }
+    for (int i = 0; i < bombas->totais; i++)
+    {
+        bombas->estados[i] = true;
+    }
+    bombas->luzAmarela = true;
+    bombas->luzVermelha = false;
+    return bombas;
+}
+
+/* Mostra ATIVA se o estado for verdadeiro, INATIVA se for
+falso. Utilizado por estadoDoBombeamento. Função local. */
+void mostrarEstado(bool estado)
+{
+    if (estado)
+    {
+        printf("ATIVA\n");
+    }
+    else
+    {
+        printf("INATIVA\n");
+    }
+}
+
+/* Mostra o estado de todos os componentes de um sistema de
+bombeamento no terminal. */
+void estadoDoBombeamento(sistemaDeBombeamento* bombas)
+{
+    printf("Bombas totais: %d\n", bombas->totais);
+    printf("Bombas ativas: %d\n", bombas->ativas);
+    printf(" Séries de bombas:\n");
+    for (int i = 0; i < bombas->totais; i++)
+    {
+        printf("  Série %d: ", i+1);
+        mostrarEstado(bombas->estados[i]);
+    }
+    printf("Luz amarela: ");
+    mostrarEstado(bombas->luzAmarela);
+    printf("Luz vermelha: ");
+    mostrarEstado(bombas->luzVermelha);
+}
+
+/* Altera o número de bombas ativas de um sistema de bombeamento,
+atualizando ao mesmo tempo o estado de cada série de bombas e da luz
+indicadora. */
+void alterarBombasAtivas(sistemaDeBombeamento* bombas, int ativas)
+{
+    // Bloqueia a ativação das bombas caso o modo de emergência
+    // esteja ativado.
+    if (bombas->luzVermelha)
+    {
+        return;
+    }
+    bombas->ativas = ativas;
+    for (int i = 0; i < bombas->totais; i++)
+    {
+        bombas->estados[i] = i < ativas;
+    }
+    bombas->luzAmarela = ativas;
+}
+
+/* Ativa o estado de emergência das bombas. */
+void emergenciaDoBombeamento(sistemaDeBombeamento* bombas)
+{
+    alterarBombasAtivas(bombas, 0);
+    bombas->luzVermelha = true;
+}
+
+/* Desativa o estado de emergência das bombas. */
+void normalizacaoDoBombeamento(sistemaDeBombeamento* bombas)
+{
+    bombas->luzVermelha = false;
+}
---
+++ eletrochallenge-final/bombas.h
@@ -0,0 +1,48 @@
+#ifndef _BOMBAS
+#define _BOMBAS
+#endif
+
+#include <stdbool.h>
+
+// Número de bombas padrão, definido pelo desafio.
+#define NUM_BOMBAS 25
+
+/** Representação programática de um sistema de bombeamento. Em um
+sistema real, os valores das variáveis estados (das bombas), luzAmarela
+e luzVermelha seriam usados para controlar os respectivos dispositivos
+mecânicos, através de uma interface controlador -> dispositivo. */
+typedef struct {
+    // Número de bombas totais.
+    int totais;
+    // Número de bombas ativas.
+    int ativas;
+    // Nos itens seguintes, true = ativa, false = inativa.
+    // Array que representa o estado de cada bomba.
+    bool *estados;
+    // Ativada quando ativas > 0.
+    bool luzAmarela;
+    // Ativada quando o botão é pressionado.
+    bool luzVermelha;
+} sistemaDeBombeamento;
+
+/** Protótipos das funções públicas, utilizadas pelo controlador
+principal. */
+
+/* Cria e inicializa um sistema de bombeamento. No início, todas as
+bombas estão ativas. */
+sistemaDeBombeamento* Bombas(int num_bombas);
+
+/* Mostra o estado de todos os componentes de um sistema de
+bombeamento no terminal. */
+void estadoDoBombeamento(sistemaDeBombeamento* bombas);
+
+/* Altera o número de bombas ativas de um sistema de bombeamento,
+atualizando ao mesmo tempo o estado de cada série de bombas e da luz
+indicadora. */
+void alterarBombasAtivas(sistemaDeBombeamento* bombas, int ativas);
+
+/* Ativa o estado de emergência das bombas. */
+void emergenciaDoBombeamento(sistemaDeBombeamento* bombas);
+
+/* Desativa o estado de emergência das bombas. */
+void normalizacaoDoBombeamento(sistemaDeBombeamento* bombas);
